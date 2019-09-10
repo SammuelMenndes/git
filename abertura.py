2c0b61276dc3d6ed98389d350fb177b22bebd673
Samuel Mendes <leumas.sm2015@gmail.com>
2019-09-10 09:59:20 -0300

    Add OS DADOS DA PRIMEIRA VIDEO AULA NO ARQUIVO abertura.py

---
+++ abertura.py
@@ -0,0 +1,24 @@
+print()
+print('!'+"=="*10, "INICIO", "=="*10+'!')
+print('''https://www.youtube.com/watch?v=FF1f4bKYhoo
+VIDEO AULA 1
+=========================
+o que é o GIT e GitHub	=
+=========================
+GIT => É um sistema de controle de versões.
+Um sistema de contre de versões => Documenta e armazena todas as alterações
+que são feitas no desenvolvimento do projeto.
+
+Porque precisanmos aprender a usar esse tipo de ferramenta?
+
+Para quem quer aprender programação essa ferramenta é tão importante quanto aprender 
+logica de programação.
+
+algumas vantagens;
+
+backup:
+controle de versões:
+trbalho em equip:
+portfolio:''')
+print('!'+"=="*10, "FIM", "=="*10+'!')
+print()
